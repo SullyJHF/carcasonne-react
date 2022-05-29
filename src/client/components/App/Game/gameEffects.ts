862f6284a3090a6c32a93567d6767b2ef318b037
Sullivan Ford <sullyjhford@gmail.com>
2022-05-29 10:28:21 +0100

    Remove reference to tiles updated socket event

--- src/client/components/App/Game/gameEffects.ts
+++ src/client/components/App/Game/gameEffects.ts
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { Socket } from 'socket.io-client';
 import { Game } from '../../../../server/sockets/models/Game';
 import { GAME_EVENTS } from '../../../../shared/constants/socketConstants';
-import { BoardPosition, tilesUpdated } from '../../../Store/BoardSlice';
 import { gameStateUpdated } from '../../../Store/GameSlice';
 import { useAppDispatch } from '../../../Store/hooks';
 import { useSocket } from '../SocketTest/socketHooks';
@@ -11,16 +10,12 @@ export const useGameEffects = () => {
   const dispatch = useAppDispatch();
   const socket = useSocket();
   const subscribe = (_socket: Socket) => {
-    _socket.on(GAME_EVENTS.TILES_UPDATED, (position: BoardPosition) => {
-      dispatch(tilesUpdated(position));
-    });
     _socket.on(GAME_EVENTS.STATE_UPDATE, (gameState: Game) => {
       console.log(gameState);
       dispatch(gameStateUpdated(gameState));
     });
   };
   const unsubscribe = (_socket: Socket) => {
-    _socket.off(GAME_EVENTS.TILES_UPDATED);
     _socket.off(GAME_EVENTS.STATE_UPDATE);
   };
 
