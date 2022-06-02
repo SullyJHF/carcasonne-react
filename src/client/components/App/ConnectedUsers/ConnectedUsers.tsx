df603ad80a30e9608bf3e2704b7e2a1042a50de1
Sullivan Ford <sullyjhford@gmail.com>
2022-06-02 17:20:40 +0100

    Show when players are disconnected

--- src/client/components/App/ConnectedUsers/ConnectedUsers.tsx
+++ src/client/components/App/ConnectedUsers/ConnectedUsers.tsx
@@ -1,12 +1,26 @@
+import ClearIcon from '@mui/icons-material/Clear';
 import React from 'react';
+import { Player as IPlayer } from '../../../../server/sockets/models/PlayerManager';
 import { useUserData } from '../../../Store/UserSlice';
+import './player.scss';
+
+const Player = ({ player }: { player: IPlayer }) => {
+  return (
+    <div className="player">
+      <div className="name">{player.name}</div>
+      <div className={`connected-pip ${player.connected ? 'connected' : 'disconnected'}`}>
+        <ClearIcon sx={{ color: 'red' }} />
+      </div>
+    </div>
+  );
+};
 
 export const ConnectedUsers = () => {
   const { users } = useUserData();
   return (
     <div id="connected-users">
       {Object.keys(users).map((userId) => (
-        <div key={userId}>{users[userId].name}</div>
+        <Player key={userId} player={users[userId]} />
       ))}
     </div>
   );
